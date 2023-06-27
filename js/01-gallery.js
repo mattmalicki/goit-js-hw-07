07af78293fb598780a94e5668c64e77df9b9409d
mattmalicki <mateusz.malicki96@gmail.com>
2023-06-27 20:10:42 +0200

    added lightbox to html file

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,10 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+galleryItems.forEach((image) => {
+  basicLightbox.create(`
+    <img src="${image.preview}" alt="${image.description}" class="gallery__item">
+`);
+});
