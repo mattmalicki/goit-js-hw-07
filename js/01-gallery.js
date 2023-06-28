a7e661ab31daa2ab688abede1f7cbcbe183d5b19
mattmalicki <mateusz.malicki96@gmail.com>
2023-06-28 19:23:05 +0200

    task 1 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,9 +2,40 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+const galleryList = document.querySelector("ul.gallery");
+galleryList.insertAdjacentHTML("beforeend", createGalleryMarkups(galleryItems));
 
-galleryItems.forEach((image) => {
-  basicLightbox.create(`
-    <img src="${image.preview}" alt="${image.description}" class="gallery__item">
+galleryList.addEventListener("click", imageClick);
+
+function createGalleryMarkups(array) {
+  let markupsString = "";
+  array.forEach((element) => {
+    markupsString += `<li class="gallery__item">
+        <a class="gallery__link" href="${element.original}">
+        <img src="${element.preview}"
+        data-source="${element.original}" 
+        alt="${element.description}"
+        class="gallery__image" />
+        </a></li>`;
+  });
+  return markupsString;
+}
+
+function imageClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="1280" />
 `);
-});
+
+  instance.show();
+
+  galleryList.addEventListener("keydown", (event) => {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  });
+}
